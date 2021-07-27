573be61b2562e48d2aef15c31ea0de9ca6004e03
dynle <doyun04180@gmail.com>
2021-07-28 02:47:10 +0900

    initial files & firebase setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import {auth} from './Utils/fbase'
 
 export default function App() {
+  const login = () => {
+    auth.createUserWithEmailAndPassword("hi@gmail.com",'123456').then(()=>{
+      console.log('account signed up') //working fine!!!!
+    })
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="account" onPress={login}>create account</Button>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/Utils/fbase.tsx
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+
+// Optionally import the services that you want to use
+import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyAkq_vCJWUdJFzV556DQ3HmnlYJ2MEmCG0',
+  authDomain: 'morning-assistant-8717d.firebaseapp.com',
+  databaseURL: 'https://morning-assistant-8717d.firebaseio.com',
+  projectId: 'morning-assistant-8717d',
+  storageBucket: 'morning-assistant-8717d.appspot.com',
+  messagingSenderId: '658921868833',
+  appId: '1:658921868833:ios:24a9931a1192a6512b9d60', //use apple app
+  measurementId: 'G-measurement-id',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
