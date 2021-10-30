82a4222b3651b835a8406b71c85775e7c1a69565
Doyoon Lee <dynleedev@gmail.com>
2021-10-31 01:32:11 +0900

    trying to show userName on prompt

--- src/Utils/UpdateDBUtil.tsx
+++ src/Utils/UpdateDBUtil.tsx
@@ -2,17 +2,20 @@ import { Alert } from "react-native";
 import { authService, dbService, UserType, firebaseInstance } from "../fbase";
 import { schedulePushNotification } from "../Utils/NotificationUtil";
 
-export const UpdateAlarmDBUtil = async (user: UserType, _settingData: any[]) => {
+export const UpdateAlarmDBUtil = async (
+    user: UserType,
+    _settingData: any[]
+) => {
     const docRef = dbService.collection("users").doc(`${user.uid}`);
     const days = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"];
-    const settingData = [null,_settingData];
+    const settingData = [null, _settingData];
     for (var i = 0; i < 7; i++) {
         await docRef.collection("alarmTime").doc(`${days[i]}`).set({
             key: settingData[1]![i].key,
             title: settingData[1]![i].title,
             meridiem: settingData[1]![i].meridiem,
             hour: settingData[1]![i].hour,
-            minutes: settingData[1]![i].minutes
+            minutes: settingData[1]![i].minutes,
 
             // original
             // hour: settingData[1]![i].hour[1],
@@ -22,28 +25,45 @@ export const UpdateAlarmDBUtil = async (user: UserType, _settingData: any[]) => 
     schedulePushNotification(settingData);
 };
 
-export const ChangeNameDBUtil = (user:UserType) => {
+export const ChangeNameDBUtil = (user: UserType) => {
+    var docRef = dbService.collection("users").doc(`${user.uid}`);
+    let docData:any;
+    docRef.get().then((doc)=>{
+        if(doc.exists){
+            docData = doc.data();
+            console.log(docData);
+        }
+    })
+
     Alert.prompt(
         "새로운 이름을 입력해 주세요.",
+        // `현재 이름: ${docData.userName}`,
         undefined,
         [
             {
-                text:"취소",
-                style:"cancel"
+                text: "취소",
+                style: "cancel",
             },
             {
-                text:"확인",
-                onPress: newName=>UpdateUserNameDBUtil(user,newName)
-            }
+                text: "확인",
+                onPress: (newName) => UpdateUserNameDBUtil(user, newName),
+            },
         ]
-    )
-}
+    );
+};
 
-const UpdateUserNameDBUtil = async (user: UserType,newName:string | undefined)=>{
-    await dbService.collection("users").doc(`${user.uid}`).update({
-        userName: newName
-    }).then(()=>{
-        console.log("user name successfully updated");
-    })
+const UpdateUserNameDBUtil = async (
+    user: UserType,
+    newName: string | undefined
+) => {
+    await dbService
+        .collection("users")
+        .doc(`${user.uid}`)
+        .update({
+            userName: newName,
+        })
+        .then(() => {
+            console.log("user name successfully updated");
+        });
     Alert.alert("업데이트 완료");
-}
+};
